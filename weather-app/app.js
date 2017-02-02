378a065c033850ad92df522f1e3af7c57d3d8cf0
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-02 13:27:06 -0800

    added commandline args with yargs

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+})
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+let formattedAddress = encodeURIComponent(argv.address);
+
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1726%2073rd%20ave%20se%20Lake%20Stevens%20wa%2098258',
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${formattedAddress}`,
   json: true
 }, (err, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2));
