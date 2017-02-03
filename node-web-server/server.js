6e9a706592f35602e4c658943fb92ef7636d1e5a
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-03 08:21:40 -0800

    added server logging

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -6,7 +6,7 @@ let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 
 app.use((req, res, next) => {
   const now = new Date().toString();
@@ -20,6 +20,12 @@ app.use((req, res, next) => {
   });
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
