b89f5dfb7ad0992886a1a6b0824e01f56ce92f4f
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-03 08:07:05 -0800

    added hbs helpers

---
+++ node-web-server/readme.md
@@ -0,0 +1,2 @@
+# Node Web Server   
+A web app served by NodeJS/Express using Handlebars as the view engine.
--- node-web-server/server.js
+++ node-web-server/server.js
@@ -7,6 +7,14 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
