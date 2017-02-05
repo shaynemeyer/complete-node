929743792fb9c677696d6803a66a26388962aac6
Shayne <shayne.meyer@gmail.com>
2017-02-04 19:21:44 -0800

    added server

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+var newTodo = new Todo({
+  text: 'Buy groceries',
+  completed: false
+});
+
+newTodo.save().then((doc) => {
+  console.log('saved todo', doc);
+}, (e) => {
+  console.log('Unable to save todo');
+});
