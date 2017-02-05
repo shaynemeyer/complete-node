7555d592b5202be1d717e5b9bbafe2842417a1fe
Shayne <shayne.meyer@gmail.com>
2017-02-04 20:49:40 -0800

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -29,6 +30,36 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  // valid id using isValid
+  if(!ObjectID.isValid(id)){
+    // 404 - send back empty
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
+    // success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    // error
+      // 400 - and send empty body back
+
 });
 
 app.listen(3000, () => {
