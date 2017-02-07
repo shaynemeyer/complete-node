e9ab3ab0e80e1c28a825634582012d7d4b71bba0
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-07 07:47:46 -0800

    initial emit and listen email messages

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -2,8 +2,18 @@ let socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
+
+  socket.emit('createEmail', {
+    to: 'test@example',
+    text: 'hello from the clientside'
+  })
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function (email) {
+  console.log('New email:', email);
+
 });
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -15,6 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
  console.log('New user connected');
 
+ socket.emit('newEmail', {
+   from: 'me@example.com',
+   text: 'hello'
+ });
+
+ socket.on('createEmail', (newEmail) => {
+   console.log('createEmail', newEmail);
+ });
+
  socket.on('disconnect', () => {
    console.log('User was disconnected');
  });
