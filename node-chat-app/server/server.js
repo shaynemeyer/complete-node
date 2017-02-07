dc16b6c34485259b77e32a9d1fc65be3791b5848
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-07 12:50:33 -0800

    added geolocation to send messages

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -25,6 +25,10 @@ io.on('connection', (socket) => {
    callback('server got it');
  });
 
+ socket.on('createLocationMessage', (coords) => {
+   io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
+ });
+
  socket.on('disconnect', () => {
    console.log('User was disconnected');
  });
