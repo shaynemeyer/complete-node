f21be723799fdb0778e98cad8a28d63a1ed9b8e4
Shayne Meyer <shayne.meyer@gmail.com>
2017-02-07 07:59:42 -0800

    Emit newMessage on createMessage

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'test@example',
-    text: 'hello from the clientside',
-  })
 });
 
 socket.on('disconnect', function() {
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
  console.log('New user connected');
 
- socket.emit('newMessage', {
-   from: 'me@example.com',
-   text: 'hello',
-   createdAt: 1234
- });
-
  socket.on('createMessage', (newMessage) => {
    console.log('createMessage', newMessage);
+   io.emit('newMessage', {
+     from: newMessage.from,
+     text: newMessage.text,
+     createdAt: new Date().getTime()
+   });
  });
 
  socket.on('disconnect', () => {
