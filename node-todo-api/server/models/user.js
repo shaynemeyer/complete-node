959cbb89909dce043ea8ffc45930f027b3db8ed9
Shayne <shayne.meyer@gmail.com>
2017-02-06 18:45:07 -0800

    Add DELETE /users/me/token route

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
