49563694ebf98b143c9332455a48b8855a4ea364
Shayne <shayne.meyer@gmail.com>
2017-02-07 17:50:47 -0800

    Scroll to bottom if user is close to bottom

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,5 +1,22 @@
 let socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  const messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+  // Heights
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -18,11 +35,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  // let formattedTime = moment(msg.createdAt).format('h:mm a');
-  // let li = jQuery('<li></li>');
-  // li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
-  //
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -35,14 +48,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  // let li = jQuery('<li></li>');
-  // let a = jQuery('<a target="_blank">My current location</a>');
-  //
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
-
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
