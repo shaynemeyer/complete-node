74d319f90f856ef9cbbc3726a1d40ec6fbb57bcc
Shayne <shayne.meyer@gmail.com>
2017-02-08 06:20:39 -0800

    added join messaging for rooms

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -17,14 +17,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
  console.log('New user connected');
 
- socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
- socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
  socket.on('join', (params, callback) => {
     if(!isRealString(params.name) || !isRealString(params.room)) {
       callback('Name and room are required!');
     }
+
+    socket.join(params.room);
+    // socket.leave('room name');
+
+    // io.emit -> io.to('room name').emit
+    // socket.broadcast.emit -> socket.broadcast.to('room name').emit
+    // socket.emit
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
 
     callback();
  });
