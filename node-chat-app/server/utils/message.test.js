35b8da4e5de0ba73960756c8151f53597b7d9945
Shayne <shayne.meyer@gmail.com>
2017-02-07 16:50:54 -0800

    format timestamps using momentjs

--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -5,7 +5,7 @@ let {generateMessage, generateLocationMessage} = require('./message');
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     const from = 'Jen', text = 'Some message', message = generateMessage(from, text);
-
+    console.log(message);
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
   });
